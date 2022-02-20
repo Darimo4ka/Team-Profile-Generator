d508452769a7a0eeb2e28bf2b61a628b4e6e87a8
Darimo4ka <darima_badm@yahoo.com>
2022-02-20 16:09:20 -0500

    create path ffor my html

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const path = require("path"); 
+//  giving where to create html when function genereateHTML called.
 
-// const path = require("path"); maybe ti add employee?
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-generateHTML();
+// const render = require("./lib/htmlRenderer");
 const team = [];
-// function initApp() {
-//   generateHTML();
-//   addToTeam();
-// }
 addToTeam();
 
 function addToTeam() {
@@ -39,10 +38,8 @@ function addToTeam() {
       internInfo();
     }
     else if (employeeRole === "Finish") {
-      generateHTML(team);
+      generateHTML();
     }
-
-    // renderTeam();
   });
 }
 
@@ -132,13 +129,7 @@ function internInfo() {
       addToTeam();
     });
 }
-
-
-
-//   //  create function to write index.html file to log output:
-// const fileName =  require('./output/index.html');
-//   
-
+ //  create function to write index.html file to log output:
 function generateHTML(){
   const html = `<!DOCTYPE html>
     <html lang="en">
@@ -155,35 +146,10 @@ function generateHTML(){
         </nav>
         <div class="container">
             <div class="row">`
-    fs.writeFile("./output/team.html", html, function(err) {
+    fs.writeFile(outputPath, html, function(err) {
         if (err) {
             console.log(err);
         }
     });
-    console.log("start");
+    console.log("HTML generated for team!");
 }
-
-
-  // write the html page
-  // use the data passed to the function to  log the data of each member of the team
-  // `${}`;
-// };
-
-  //   function renderTeam() {
-  //     // console.log(team)
-  //     const html=htmlPageContent(team)
-      
-  //   // fs.writeFile('team.html',html, (err)=>
-  //   //     err? console.log(err) : console.log("success")
-  //   // )
-  // }
-
-//     const html = ``;
-//     fs.writeFile("./output/team.html", html, function(err) {
-//         if (err) {
-//             console.log(err);
-//         }
-//     });
-//     console.log("start");
-// }
-// initApp()
